90cd33ddbd52756014e5a0ce4ca6548b4bc5c56d
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-17 00:27:04 -0300

    Abstraindo em 3 camadas

---
+++ src/Master.Rotas.Business/Interfaces/INotificador.cs
@@ -0,0 +1,11 @@
+﻿using Master.Rotas.Business.Notification;
+
+namespace Master.Rotas.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
---
+++ src/Master.Rotas.Business/Models/Rota.cs
@@ -0,0 +1,10 @@
+﻿namespace Master.Rotas.Business.Models
+{
+    public class Rota : Entity
+    {
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+        public double Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
