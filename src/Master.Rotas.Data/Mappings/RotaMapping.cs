07de05bcc3b22c5b71bb0a63f71478f7ec31021d
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-17 00:27:04 -0300

    Abstraindo em 3 camadas

---
+++ src/Master.Rotas.Data/Mappings/RotaMapping.cs
@@ -0,0 +1,24 @@
+﻿using Master.Rotas.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Master.Rotas.Data.Mappings
+{
+    public class RotaMapping : IEntityTypeConfiguration<Rota>
+    {
+        public void Configure(EntityTypeBuilder<Rota> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Origem)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(p => p.Destino)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.ToTable("Rotas");
+        }
+    }
+}
