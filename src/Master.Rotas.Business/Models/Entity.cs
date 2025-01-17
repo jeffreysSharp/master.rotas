f4c928202563341a1b0bc2f27f2ff2628ce5e7c2
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-17 00:27:04 -0300

    Abstraindo em 3 camadas

---
+++ src/Master.Rotas.Business/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Master.Rotas.Business.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Master.Rotas.Business/Notification/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace Master.Rotas.Business.Notification
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get; }
+    }
+}
