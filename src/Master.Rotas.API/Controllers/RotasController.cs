174ab9dc4123de8c38de313aa1c6bf97c1332b0b
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-17 03:16:22 -0300

    #5 - Modelar a controller de Rotas - feature/sp1/#5

---
+++ src/Master.Rotas.API/Controllers/RotasController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Master.Rotas.API.ViewModels;
+using Master.Rotas.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Master.Rotas.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class RotasController : MainController
+    {
+        private readonly IRotaRepository _rotaRepository;
+        private readonly IMapper _mapper;
+
+        public RotasController(IRotaRepository rotaRepository,
+                               IMapper mapper)
+        {
+            _rotaRepository = rotaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<RotaViewModel>> ObterTodos()
+        {
+            var rota = _mapper.Map<IEnumerable<RotaViewModel>>(await _rotaRepository.ObterTodos());
+
+            return rota;
+        }
+    }
+}
