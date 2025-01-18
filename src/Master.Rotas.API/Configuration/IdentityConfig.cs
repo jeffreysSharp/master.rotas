9ed952ddf2c607bf557d263ea39ff7d766e063e3
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-17 22:39:57 -0300

    #13 - Implementar o JWT  - feature/sp1/#13

--- src/Master.Rotas.API/Configuration/IdentityConfig.cs
+++ src/Master.Rotas.API/Configuration/IdentityConfig.cs
@@ -1,7 +1,10 @@
 ﻿using Master.Rotas.API.Data;
 using Master.Rotas.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Master.Rotas.API.Configuration
 {
@@ -17,6 +20,33 @@ namespace Master.Rotas.API.Configuration
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders();
+
+            // JWT
+
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
 
             return services;
         }
